237d8de55eb2a096d903b94529f754eb84f7b5b0
dzryk <jamie@figment.space>
2021-08-21 17:46:34 +0000

    Add VSE++ loss and masking.

--- model.py
+++ model.py
@@ -23,14 +23,43 @@ def offgrad(model, ft):
     return model
 
 
+class VSELoss(nn.Module):
+    def __init__(self, hparams):
+        super(VSELoss, self).__init__()
+        self.hparams = hparams
+    
+    def loss(self, x, y, mask, device):
+        """Maximum violation contrastive loss (VSE++)."""
+
+        # Compute similarity scores and expand
+        scores = mask * x.mm(y.t())
+        diagonal = scores.diag().view(x.size(0), 1)
+        d1 = diagonal.expand_as(scores)
+        d2 = diagonal.t().expand_as(scores)
+
+        cost_y = (self.hparams.margin + scores - d1).clamp(min=0)
+        cost_x = (self.hparams.margin + scores - d2).clamp(min=0)
+
+        # Clear diagonals
+        iden = torch.eye(scores.size(0), device=device)
+        I = Variable(iden > .5)
+        cost_y = cost_y.masked_fill_(I, 0)
+        cost_x = cost_x.masked_fill_(I, 0)
+
+        # Maximum violation loss (VSE++)
+        cost_y = cost_y.max(1)[0]
+        cost_x = cost_x.max(0)[0]
+        return cost_y.sum() + cost_x.sum()
+
+
 class XELoss(nn.Module):
     def __init__(self, hparams):
         super(XELoss, self).__init__()
         self.hparams = hparams
 
-    def loss(self, x, y, device):
+    def loss(self, x, y, mask, device):
         """XELoss from CLIP."""
-        scores = x.mm(y.t()) / self.hparams.temp
+        scores = mask * x.mm(y.t()) / self.hparams.temp
         ground_truth = torch.arange(len(scores)).type_as(scores).long()
         cost_x = F.cross_entropy(scores, ground_truth)
         cost_y = F.cross_entropy(scores.t(), ground_truth)
@@ -64,6 +93,9 @@ class Model(pl.LightningModule):
         self.text_projection = clip_model.text_projection
         self.W = nn.Linear(TXTDIM, VISDIM)
         self.loss = XELoss(self.hparams)
+        self.loss = LOSS_MAPPING[self.hparams.loss_fn](
+            hparams=self.hparams)
+
 
     def encode_context(self, text):
         text = clip.tokenize(text, truncate=True).to(self.device)
@@ -105,12 +137,23 @@ class Model(pl.LightningModule):
         x = x[torch.arange(x.shape[0]), text.argmax(dim=-1)] @ self.text_projection
         return x
 
+
+    def compute_mask(self, text):
+        mask = np.zeros((len(text), len(text)))
+        for i in range(len(text)):
+            for j in range(len(text)):
+                mask[i,j] = text[i] == text[j]
+        mask = (1 - mask) + np.eye(len(text))
+        return torch.tensor(mask).to(self.device)
+
+
     def compute_loss(self, image, context, target):
+        mask = self.compute_mask(target)
         source = self.encode_image(image, context)
         target = self.encode_text(target)
         source = source / source.norm(dim=-1, keepdim=True)
         target = target / target.norm(dim=-1, keepdim=True)
-        return self.loss.loss(source, target, device=self.device)
+        return self.loss.loss(source, target, mask, device=self.device)
 
     def unpack(self, batch):
         x, y = batch
@@ -139,3 +182,11 @@ class Model(pl.LightningModule):
             optimizer=optimizer,
             T_max=self.hparams.tmax)
         return [optimizer], [scheduler]
+
+
+LOSS_MAPPING = OrderedDict(
+    [
+        ('vse', VSELoss),
+        ('xe', XELoss),
+    ]
+)
